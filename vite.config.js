5caa2f84bf1b1fb1c65e7ea57ce3ed585b7fce1b
nimmiev <nimmiev222@gmail.com>
2024-11-04 01:10:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Online-product-list-details/",
+  base: "/list-online-productsdetail/",
   plugins: [react()],
 })
