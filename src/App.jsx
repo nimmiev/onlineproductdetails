6b48f33358960a1f94bbd3f6105048db8722d35a
nimmiev <nimmiev222@gmail.com>
2024-11-04 21:35:33 +0530

    hash add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import NavigationBar from './components/NavigationBar';
